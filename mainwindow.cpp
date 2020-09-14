5a942b423c27e5c5acea8a817232a54994420600
MaxFleur <bakefish@web.de>
2020-09-14 18:08:09 +0000

    Adds first version of dropdown menu

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,70 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+MainWindow::MainWindow()
 {
+    // Set the main widget and create menus and actions
+    widget = new QWidget;
+    setCentralWidget(widget);
+
+    createActions();
+    createMenus();
+
+    setWindowTitle("Light Combat Manager");
+    setMinimumSize(1280, 720);
+}
+
+void MainWindow::newFile()
+{
+}
+
+// Display about message
+void MainWindow::about()
+{
+    QMessageBox::about(this, tr("About Light Combat Manager"), 
+            "Light Combat Manager. A simple Combat Manager for DnD-like games. "
+            "\nCode available on Github: https://github.com/MaxFleur/LightCombatManager"
+            "\nVersion 0.0.2 alpha.");
+}
+
+void MainWindow::aboutQt()
+{
+}
+
+// Create the user interface actions
+void MainWindow::createActions()
+{
+    // Action to start a new combat
+    newAct = new QAction(tr("&New Combat"), this);
+    newAct->setShortcuts(QKeySequence::New);
+    newAct->setStatusTip(tr("Start a new combat."));
+    connect(newAct, &QAction::triggered, this, &MainWindow::newFile);
+    // See information about the program
+    aboutAct = new QAction(tr("&About"), this);
+    aboutAct->setStatusTip(tr("About Light Combat Manager"));
+    connect(aboutAct, &QAction::triggered, this, &MainWindow::about);
+    // See information about Qt
+    aboutQtAct = new QAction(tr("About &Qt"), this);
+    aboutQtAct->setStatusTip(tr("About QT version and license"));
+    connect(aboutQtAct, &QAction::triggered, qApp, &QApplication::aboutQt);
+}
+
+// Create the dropdown menus
+void MainWindow::createMenus()
+{
+    // File menu for starting, saving and opening a combat
+    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu->addAction(newAct);
+    fileMenu->addSeparator();
+    // Help menu, displays information about the program
+    helpMenu = menuBar()->addMenu(tr("&Help"));
+    helpMenu->addAction(aboutAct);
+    helpMenu->addAction(aboutQtAct);
 }
 
 MainWindow::~MainWindow()
 {
+    delete widget;
+    delete newAct;
+    delete aboutAct;
+    delete aboutQtAct;
 }
-
--- mainwindow.h
+++ mainwindow.h
@@ -2,13 +2,36 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtWidgets>
 
+// This class handles the creation and managing of the main GUI window.
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow();
     ~MainWindow();
+    
+private slots:
+    // Slots for the dropdown menu actions
+    void newFile();
+    void about();
+    void aboutQt();
+
+private:
+    // Create the user actions and menus
+    void createActions();
+    void createMenus();
+    
+    // The main widget used for the GUI
+    QWidget *widget;
+    // The dropdown menus
+    QMenu *fileMenu;
+    QMenu *helpMenu;
+    // The actions when clicking the dropdown menu options
+    QAction *newAct;
+    QAction *aboutAct;
+    QAction *aboutQtAct;
 };
-#endif // MAINWINDOW_H
+#endif 

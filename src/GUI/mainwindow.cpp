c802484465f9fd9fb70f3403fde88102383d37d6
Bakefish <bakefish@web.de>
2020-09-20 00:28:28 +0200

    Finishes character storing feature

--- src/GUI/mainwindow.cpp
+++ src/GUI/mainwindow.cpp
@@ -47,7 +47,7 @@ void MainWindow::about()
             "Light Combat Manager. A simple Combat Manager for DnD-like games. Code available on Github:"
             "\nhttps://github.com/MaxFleur/LightCombatManager"
             "\n"
-            "\nVersion 0.2.10 alpha.");
+            "\nVersion 0.3.0 alpha.");
 }
 
 void MainWindow::aboutQt()

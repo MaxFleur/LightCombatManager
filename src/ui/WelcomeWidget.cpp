625fb8beea0a72ef8cba8fec8872824cef5b3400
MaxFleur <bakefish@web.de>
2021-12-09 22:01:09 +0100

    Typo fix in Welcome Widget

--- src/ui/WelcomeWidget.cpp
+++ src/ui/WelcomeWidget.cpp
@@ -9,7 +9,7 @@ WelcomeWidget::WelcomeWidget(QWidget *parent)
 	auto *const welcomeLabel =
 		new QLabel(
 			tr(
-				"Welcome to Light Combat Manager! \n To start a new Combat, click on 'File' -> New Combat' or hit Ctrl + N."));
+				"Welcome to Light Combat Manager! \n To start a new Combat, click on 'File' -> 'New Combat' or hit Ctrl + N."));
 	welcomeLabel->setAlignment(Qt::AlignCenter);
 
 	auto *const mainLayout = new QVBoxLayout(this);

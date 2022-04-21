258d3a27bccfd15b0cbf8a358e2c0ff7446c50c2
MaxFleur <bakefish@web.de>
2022-04-21 22:47:45 +0200

    Adjust about info

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -259,7 +259,7 @@ MainWindow::about()
 	QMessageBox::about(
 		this,
 		tr("About Light Combat Manager"),
-		tr("<p>Light Combat Manager. A simple Combat Manager for Pathfinder 1e.<br>"
+		tr("<p>Light Combat Manager. A small and simple Combat Manager for D&D-like games.<br>"
 		   "<a href='https://github.com/MaxFleur/LightCombatManager'>Code available on Github.</a></p>"
 		   "<p>Version 1.5.1. <br>"
 		   "<a href='https://github.com/MaxFleur/LightCombatManager/releases'>Changelog</a></p>"));

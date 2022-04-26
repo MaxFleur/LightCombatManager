5822458a5804259e00e0ee71761411759fefe3f6
MaxFleur <bakefish@web.de>
2022-04-26 21:30:42 +0200

    Bump version number

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -256,7 +256,7 @@ MainWindow::about()
 		tr("About Light Combat Manager"),
 		tr("<p>Light Combat Manager. A small and simple Combat Manager for D&D-like games.<br>"
 		   "<a href='https://github.com/MaxFleur/LightCombatManager'>Code available on Github.</a></p>"
-		   "<p>Version 1.6.0. <br>"
+		   "<p>Version 1.6.1. <br>"
 		   "<a href='https://github.com/MaxFleur/LightCombatManager/releases'>Changelog</a></p>"));
 }
 

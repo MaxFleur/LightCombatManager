140b2ab2a2387c01be204b5d73238c451ad0ac5b
Bakefish <bakefish@web.de>
2023-03-11 23:14:06 +0100

    Adjust about description

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -238,7 +238,7 @@ void
 MainWindow::about()
 {
     QMessageBox::about(this, tr("About Light Combat Manager"),
-                       tr("<p>Light Combat Manager. A small and simple Combat Manager for D&D-like games.<br>"
+                       tr("<p>Light Combat Manager. A small, lightweight Combat Manager for d20-based role playing games.<br>"
                           "<a href='https://github.com/MaxFleur/LightCombatManager'>Code available on Github.</a></p>"
                           "<p>Version 1.9.1.<br>"
                           "<a href='https://github.com/MaxFleur/LightCombatManager/releases'>Changelog</a></p>"));

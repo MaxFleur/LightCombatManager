867d93f4e9ca44235675c800f3b8e078f83fd7b3
Bakefish <bakefish@web.de>
2023-05-28 13:30:41 +0200

    Add CI

--- test/main.cpp
+++ test/main.cpp
@@ -6,6 +6,12 @@
 int
 main(int argc, char **argv)
 {
+#ifdef _WIN32
+    _putenv("QT_QPA_PLATFORM", "offscreen");
+#else
+    setenv("QT_QPA_PLATFORM", "offscreen", 0);
+#endif
+
     QApplication app(argc, argv);
 
     return Catch::Session().run(argc, argv);

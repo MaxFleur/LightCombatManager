b4a300b12f9fdd1a14a51d74dd82f4cba5b97061
Bakefish <bakefish@web.de>
2024-03-24 16:30:15 +0100

    [Redesign] Beautify Welcoming Widget Screen

--- src/ui/WelcomeWidget.hpp
+++ src/ui/WelcomeWidget.hpp
@@ -1,10 +1,23 @@
 #pragma once
 
+#include <QPointer>
 #include <QWidget>
+
+class QSvgWidget;
 
 // This class creates the welcome text
 class WelcomeWidget : public QWidget {
 public:
     explicit
     WelcomeWidget(QWidget *parent = 0);
+
+private:
+    void
+    setSvgWidgetIcon();
+
+    bool
+    event(QEvent *event) override;
+
+private:
+    QPointer<QSvgWidget> m_svgWidget;
 };

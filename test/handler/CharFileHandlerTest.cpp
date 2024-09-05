e71a674df621ac19e36c0d64f66cb8f425748175
Bakefish <bakefish@web.de>
2024-09-05 17:36:36 +0200

    [restructure] Chars now stored in separate chars folder

--- test/handler/CharFileHandlerTest.cpp
+++ test/handler/CharFileHandlerTest.cpp
@@ -7,23 +7,18 @@
 #include <QDir>
 #include <QFile>
 #include <QJsonDocument>
-#include <QSettings>
-
-#include <filesystem>
 
 TEST_CASE("CharFileHandler Testing", "[CharFileHandler]") {
     auto const charFileHandler = std::make_shared<CharFileHandler>();
     const auto character = CharacterHandler::Character("test", 0, 0, 10, false, AdditionalInfoData{ {}, "Haste" });
     const auto tableSaved = charFileHandler->writeToFile(character);
 
-    QSettings settings;
-    QDir dir(settings.fileName());
-    dir.cdUp();
-    const auto charPath = dir.absolutePath() + "/test.char";
+    QDir dir;
+    const auto charPath = dir.currentPath() + "/chars/test.char";
 
     SECTION("Char successfully saved") {
         REQUIRE(tableSaved == true);
-        REQUIRE(std::filesystem::exists(charPath.toStdString()));
+        REQUIRE(dir.exists(charPath));
     }
     SECTION("File format and content correct") {
         const auto codeCSVStatus = charFileHandler->getStatus("test.char");
@@ -45,12 +40,12 @@ TEST_CASE("CharFileHandler Testing", "[CharFileHandler]") {
 
         // Write to file
         auto byteArray = QJsonDocument(jsonObject).toJson();
-        QFile fileOut(dir.absolutePath() + "/broken.char");
+        QFile fileOut(dir.currentPath() + "/chars/broken.char");
         fileOut.open(QIODevice::WriteOnly);
         fileOut.write(byteArray);
 
         REQUIRE(charFileHandler->getStatus("broken.char") == 1);
-        std::filesystem::remove(dir.absolutePath().toStdString() + "/broken.char");
+        dir.remove(dir.currentPath() + "/chars/broken.char");
     }
     SECTION("Not existing file") {
         const auto codeCSVStatus = charFileHandler->getStatus("nonexisting.char");
@@ -59,6 +54,6 @@ TEST_CASE("CharFileHandler Testing", "[CharFileHandler]") {
     SECTION("Check file removal") {
         const auto fileRemoved = charFileHandler->removeCharacter("test.char");
         REQUIRE(fileRemoved == true);
-        REQUIRE(!std::filesystem::exists(charPath.toStdString()));
+        REQUIRE(!dir.exists(charPath));
     }
 }
